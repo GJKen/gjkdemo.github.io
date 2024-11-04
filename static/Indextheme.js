13501019ce20023d2a10ac6ab6950261efed31e6
GJKen <gjkenryan@gmail.com>
2024-11-04 18:50:04 +0800

    update 2024/11/04 18:50

--- static/Indextheme.js
+++ static/Indextheme.js
@@ -1,12 +1,6 @@
 let currentUrl = window.location.pathname;
-console.log("输出当前路径:", currentUrl); // 输出当前路径
-
-if (currentUrl.includes('/index.html') || currentUrl === "/") {
-    console.log('应用主页主题'); // 当路径符合条件时输出
-
     // 应用主页主题样式
     let style = document.createElement("style");
-    console.log('修改样式');
     style.innerHTML = `
         /* header样式 */
         #header {
@@ -23,7 +17,4 @@ if (currentUrl.includes('/index.html') || currentUrl === "/") {
         }
     `;
     document.head.appendChild(style);
-    console.log('已改样式');
-} else {
-    console.log('未应用主题'); // 当路径不符合条件时输出
 }
