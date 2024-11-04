7faf82345db4faf8fbe13add775cd9e5bf9f40ae
GJKen <gjkenryan@gmail.com>
2024-11-04 18:18:09 +0800

    update 2024/11/04 18:18

--- docs/Indextheme.js
+++ docs/Indextheme.js
@@ -1,11 +1,12 @@
 let currentUrl = window.location.pathname;
-console.log("Current URL Pathname:", currentUrl); // 输出当前路径
+console.log("输出当前路径:", currentUrl); // 输出当前路径
 
 if (currentUrl.includes('/index.html') || currentUrl === "/") {
     console.log('应用主页主题'); // 当路径符合条件时输出
 
     // 应用主页主题样式
     let style = document.createElement("style");
+    console.log('修改样式');
     style.innerHTML = `
         /* header样式 */
         #header {
@@ -22,6 +23,7 @@ if (currentUrl.includes('/index.html') || currentUrl === "/") {
         }
     `;
     document.head.appendChild(style);
+    console.log('已改样式');
 } else {
     console.log('未应用主题'); // 当路径不符合条件时输出
 }
