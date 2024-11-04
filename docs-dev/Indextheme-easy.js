f73f4132136f394128926cecb6f368b2aaa50359
GJKen <gjkenryan@gmail.com>
2024-11-04 23:20:16 +0800

    update 2024/11/04 23:20

--- docs-dev/Indextheme-easy.js
+++ docs-dev/Indextheme-easy.js
@@ -11,28 +11,4 @@ applyStyles(`
 		background: url('https://ipfs.decentralized-content.com/ipfs/QmQn8HcHeutmaH2qEbPK8PT6TXEpj58VizavBxanCiQcYv') no-repeat center center fixed;
 		background-size: cover;
 	}
-	#header {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 10px;
-		padding: 15px 0;
-	}
-	#header h1{
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 15px;
-	}
-	#header h1 a{
-		margin:unset;
-	}
-	.title-right{
-		margin:unset;
-		
-	}
-	.avatar:hover {
-		transform: scale(1.5) rotate(720deg);
-		box-shadow: 0 0 10px rgb(45 250 255 / 74%);
-	}
 `);
