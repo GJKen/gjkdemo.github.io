e416e9e21ce8e98c30fd2b47ded454e39b2cffe7
gjken <gjkenryan@gmail.com>
2024-11-23 16:22:05 +0800

    update 2024/11/23 16:22

---
+++ static/ArticleJs.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// 引入fancybox所需的css文件以及所需的绑定函数
+	document.head.appendChild(Object.assign(document.createElement('link'), {
+		rel: 'stylesheet',
+		href: 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css'
+	}));
+	Fancybox.bind('[data-fancybox="gallery"]', {});
+
+	// https://lazyload.js.org
+	// 延迟加载图片所需函数
+	const lazyImage = new LazyLoadImage('.lazy-load-image');
+	// Customize the loading strategy
+	lazyImage.observeWithIntersectionObserver();
+	// Optional: Provide a placeholder image
+	lazyImage.placeholderImage = 'placeholder.jpg';
+	lazyImage.init();
+});
