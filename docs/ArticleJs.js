0ddf1ac4be850a06f1ab5fff933f82aa5ea7d7dc
GJKen <null>
2024-11-23 09:50:36 +0000

    🎉auto update by Gmeek action

--- docs/ArticleJs.js
+++ docs/ArticleJs.js
@@ -1,3 +1,10 @@
+// 图片懒加载所需函数
+const lazyImage = new LazyImage('.lazy-load-image');
+// Customize the loading strategy
+lazyImage.observeWithIntersectionObserver();
+// Optional: Provide a placeholder image
+lazyImage.placeholderImage = 'placeholder.jpg';
+lazyImage.init();
 document.addEventListener('DOMContentLoaded', () => {
 	// 引入fancybox所需的css文件以及所需的绑定函数
 	document.head.appendChild(Object.assign(document.createElement('link'), {
@@ -6,10 +13,3 @@ document.addEventListener('DOMContentLoaded', () => {
 	}));
 	Fancybox.bind('[data-fancybox="gallery"]', {});
 });
-// 图片懒加载所需函数
-const lazyImage = new LazyImage('.lazy-load-image');
-// Customize the loading strategy
-lazyImage.observeWithIntersectionObserver();
-// Optional: Provide a placeholder image
-lazyImage.placeholderImage = 'placeholder.jpg';
-lazyImage.init();
