71b065f9caafa814679c83ffc67bacd30d41249e
gjken <gjkenryan@gmail.com>
2024-12-03 17:29:33 +0800

    update 2024/12/03 17:29

--- static/ArticleJs.js
+++ static/ArticleJs.js
@@ -1,10 +1,3 @@
-// 图片懒加载所需函数
-const lazyImage = new LazyImage('.lazy-load-image');
-// Customize the loading strategy
-lazyImage.observeWithIntersectionObserver();
-// Optional: Provide a placeholder image
-lazyImage.placeholderImage = 'placeholder.jpg';
-lazyImage.init();
 document.addEventListener('DOMContentLoaded', () => {
 	// 引入fancybox所需的css文件以及所需的绑定函数
 	document.head.appendChild(Object.assign(document.createElement('link'), {
