483e3f256b52c805b0f1b6185647d7ff2d726a74
gjken <gjkenryan@gmail.com>
2024-12-04 11:37:27 +0800

    update 2024/12/04 11:37

--- static/ArticleJs.js
+++ static/ArticleJs.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const ob = new IntersectionObserver((entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const img = entry.target;
+				img.src = img.getAttribute('img-src'); // 获取 img-src 属性的值
+				img.classList.remove('ImgLazyLoad');
+				ob.unobserve(img);
+			}
+		});
+	}, {
+		rootMargin: '0px 0px 500px 0px',
+	});
+
+	const imgs = document.querySelectorAll('[img-src]'); // 选择所有具有 img-src 属性的元素
+	imgs.forEach(img => {
+		ob.observe(img);
+	});
+
 	// 引入fancybox所需的css文件以及所需的绑定函数
 	document.head.appendChild(Object.assign(document.createElement('link'), {
 		rel: 'stylesheet',
