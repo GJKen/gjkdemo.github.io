9b88b52467b43e45c94668414a24acc81535c8f9
GJKen <gjkenryan@gmail.com>
2024-12-12 23:50:17 +0800

    update 2024/12/12 23:50

--- static/ArticleJs.js
+++ static/ArticleJs.js
@@ -1,44 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const ob = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				const img = entry.target;
-				img.src = img.getAttribute('img-src'); // 获取 img-src 属性的值
-				ob.unobserve(img);
-				setTimeout(() => {
-					img.classList.remove('ImgLazyLoad'); // 增加图片显示延迟, 延迟过程中图片也是会加载的
-					img.classList.add('ImgLoaded') //增加图片显示后的类名
-				}, 500);
-			}
-		});
-	}, {
-		rootMargin: '0px 0px 500px 0px',
-	});
+function handleLazyLoad(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.getAttribute('img-src');
+            observer.unobserve(img);
+            setTimeout(() => {
+                img.classList.remove('ImgLazyLoad');
+                img.classList.add('ImgLoaded');
+            }, 500);
+        }
+    });
+}
 
-	const imgs = document.querySelectorAll('[img-src]'); // 选择所有具有 img-src 属性的元素
-	imgs.forEach(img => {
-		ob.observe(img);
-		// 图片加载失败时的处理
-		img.onerror = function() {
-			img.classList.remove('ImgLazyLoad'); // 移除 ImgLazyLoad 类名
-			// 创建一个容器 div
-			const errorContainer = document.createElement('div');
-			errorContainer.classList.add('Imgerror-container');
-			// SVG 内容
-			const svgContent =
-				`<svg xmlns="http://www.w3.org/2000/svg" style="height:60px;" class="Imgerror" viewBox="0 0 1024 1024"><path fill="#ff5b5b" d="M320 896h-77.833L515.92 622.253a21.333 21.333 0 0 0 3.16-26.133l-89.427-149.053 165.427-330.86A21.333 21.333 0 0 0 576 85.333H96a53.393 53.393 0 0 0-53.333 53.334v746.666A53.393 53.393 0 0 0 96 938.667h224A21.333 21.333 0 0 0 320 896zM96 128h445.48L386.253 438.46a21.333 21.333 0 0 0 .787 20.513L474 603.86l-69.333 69.333-89.62-89.653a53.333 53.333 0 0 0-75.427 0L85.333 737.827v-599.16A10.667 10.667 0 0 1 96 128zM85.333 885.333v-87.166l184.46-184.454a10.667 10.667 0 0 1 15.08 0l89.627 89.62L181.833 896H96a10.667 10.667 0 0 1-10.667-10.667zm192-458.666C336.147 426.667 384 378.813 384 320s-47.853-106.667-106.667-106.667S170.667 261.187 170.667 320s47.853 106.667 106.666 106.667zm0-170.667a64 64 0 1 1-64 64 64.073 64.073 0 0 1 64-64zM928 128H661.333a21.333 21.333 0 0 0-19.08 11.793l-.046.087c-.04.087-.087.173-.127.253L535.587 353.127a21.333 21.333 0 1 0 38.16 19.08l100.773-201.54H928a10.667 10.667 0 0 1 10.667 10.666V652.5L783.713 497.54a53.333 53.333 0 0 0-75.426 0L571.08 634.747a21.333 21.333 0 0 0-3.153 26.153l24.666 41.08-203.646 244.36a21.333 21.333 0 0 0 16.386 34.993H928A53.393 53.393 0 0 0 981.333 928V181.333A53.393 53.393 0 0 0 928 128zm0 810.667H450.88L635.053 717.66a21.333 21.333 0 0 0 1.907-24.667l-23.933-39.886L738.46 527.68a10.667 10.667 0 0 1 15.08 0l185.127 185.153V928A10.667 10.667 0 0 1 928 938.667z"/></svg>`;
-			const pContent = `<p class="Imgerror-message">图片错误</p>`;
+function handleError(img) {
+    img.classList.remove('ImgLazyLoad');
+    const errorContainer = document.createElement('div');
+    errorContainer.classList.add('Imgerror-container');
+    errorContainer.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" style="height:60px;" class="Imgerror" viewBox="0 0 1024 1024"><path fill="#ff5b5b" d="M320 896h-77.833L515.92 622.253a21.333 21.333 0 0 0 3.16-26.133l-89.427-149.053 165.427-330.86A21.333 21.333 0 0 0 576 85.333H96a53.393 53.393 0 0 0-53.333 53.334v746.666A53.393 53.393 0 0 0 96 938.667h224A21.333 21.333 0 0 0 320 896zM96 128h445.48L386.253 438.46a21.333 21.333 0 0 0 .787 20.513L474 603.86l-69.333 69.333-89.62-89.653a53.333 53.333 0 0 0-75.427 0L85.333 737.827v-599.16A10.667 10.667 0 0 1 96 128zM85.333 885.333v-87.166l184.46-184.454a10.667 10.667 0 0 1 15.08 0l89.627 89.62L181.833 896H96a10.667 10.667 0 0 1-10.667-10.667zm192-458.666C336.147 426.667 384 378.813 384 320s-47.853-106.667-106.667-106.667S170.667 261.187 170.667 320s47.853 106.667 106.666 106.667zm0-170.667a64 64 0 1 1-64 64 64.073 64.073 0 0 1 64-64zM928 128H661.333a21.333 21.333 0 0 0-19.08 11.793l-.046.087c-.04.087-.087.173-.127.253L535.587 353.127a21.333 21.333 0 1 0 38.16 19.08l100.773-201.54H928a10.667 10.667 0 0 1 10.667 10.666V652.5L783.713 497.54a53.333 53.333 0 0 0-75.426 0L571.08 634.747a21.333 21.333 0 0 0-3.153 26.153l24.666 41.08-203.646 244.36a21.333 21.333 0 0 0 16.386 34.993H928A53.393 53.393 0 0 0 981.333 928V181.333A53.393 53.393 0 0 0 928 128zm0 810.667H450.88L635.053 717.66a21.333 21.333 0 0 0 1.907-24.667l-23.933-39.886L738.46 527.68a10.667 10.667 0 0 1 15.08 0l185.127 185.153V928A10.667 10.667 0 0 1 928 938.667z"/></svg>
+        <p class="Imgerror-message">图片错误</p>`;
+    img.parentNode.insertBefore(errorContainer, img.nextSibling);
+    img.style.display = 'none';
+}
 
-			// 将 SVG 内容和 <p> 标签插入到 errorContainer div 内
-			errorContainer.innerHTML = svgContent + pContent;
+const ob = new IntersectionObserver(handleLazyLoad, {
+    rootMargin: '0px 0px 500px 0px',
+});
 
-			// 将 errorContainer 插入到 img 元素的同一父元素下
-			img.parentNode.insertBefore(errorContainer, img.nextSibling);
+const imgs = document.querySelectorAll('[img-src]');
+imgs.forEach(img => {
+    ob.observe(img);
+    img.onerror = () => handleError(img);
+});
 
-			// 隐藏 img 元素
-			img.style.display = 'none';
-		};
-	});
 
 	// 引入fancybox所需的css文件以及所需的绑定函数
 	document.head.appendChild(Object.assign(document.createElement('link'), {
